7a9ecd491dca040b63a4bcbbcc46d02f64e389c8
Daniish-Qureshi <mrdanish7806@gmail.com>
2026-03-14 08:32:25 +0530

    Fix socket event names mismatch

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,4 +1,5 @@
 const Message = require('../models/Message');
+const Notification = require('../models/Notification');
 
 const socketHandler = (io) => {
   const onlineUsers = new Map();
@@ -7,13 +8,14 @@ const socketHandler = (io) => {
     console.log('🟢 User connected:', socket.id);
 
     // User join
-    socket.on('user-join', (userId) => {
+    socket.on('join', (userId) => {
       onlineUsers.set(userId, socket.id);
-      io.emit('online-users', Array.from(onlineUsers.keys()));
+      io.emit('onlineUsers', Array.from(onlineUsers.keys()));
+      console.log('User joined:', userId);
     });
 
     // Send message
-    socket.on('send-message', async (data) => {
+    socket.on('sendMessage', async (data) => {
       try {
         const message = await Message.create({
           sender: data.senderId,
@@ -23,9 +25,9 @@ const socketHandler = (io) => {
 
         const receiverSocket = onlineUsers.get(data.receiverId);
         if (receiverSocket) {
-          io.to(receiverSocket).emit('receive-message', message);
+          io.to(receiverSocket).emit('receiveMessage', message);
         }
-        socket.emit('message-sent', message);
+        socket.emit('receiveMessage', message);
       } catch (error) {
         console.log('Message error:', error);
       }
@@ -34,12 +36,12 @@ const socketHandler = (io) => {
     // Typing
     socket.on('typing', (data) => {
       const receiverSocket = onlineUsers.get(data.receiverId);
-      if (receiverSocket) io.to(receiverSocket).emit('user-typing', data.senderId);
+      if (receiverSocket) io.to(receiverSocket).emit('typing', data.senderId);
     });
 
-    socket.on('stop-typing', (data) => {
+    socket.on('stopTyping', (data) => {
       const receiverSocket = onlineUsers.get(data.receiverId);
-      if (receiverSocket) io.to(receiverSocket).emit('user-stop-typing', data.senderId);
+      if (receiverSocket) io.to(receiverSocket).emit('stopTyping', data.senderId);
     });
 
     // Disconnect
@@ -47,7 +49,7 @@ const socketHandler = (io) => {
       onlineUsers.forEach((socketId, userId) => {
         if (socketId === socket.id) onlineUsers.delete(userId);
       });
-      io.emit('online-users', Array.from(onlineUsers.keys()));
+      io.emit('onlineUsers', Array.from(onlineUsers.keys()));
       console.log('🔴 User disconnected:', socket.id);
     });
   });
